1b39f386709ac4b62362c4a432e0a20d390367a3
AragonJ13 <jeanaragon0@gmail.com>
2023-12-09 19:13:55 -0800

    completed prep frontend

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,22 +1,47 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
+import React, { useState} from "react"
+import { Button, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter  } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
+
+
+
+ 
 const App = () => {
+  const [isModalOpen, setModalOpen] = useState(false)
+  const [userName, setUserName] = useState("")
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  }
+
+  const handleNameChange = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const resetInput = () => {
+    setUserName("")
+  }
+
   return (
-    <div className="entire-content">
+   
+    <div className="entire-content background-image">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input type="text" id="name" value={userName} onChange={handleNameChange} />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div className="button-container">
+         <Button  onClick={toggleModal}>Click Me</Button>
+          <Button  onClick={resetInput}>Reset</Button>
+        </div>
+        <ModalComponent  isOpen={isModalOpen} toggleModal={toggleModal} userName={userName}/>
+         
       </div>
+     
     </div>
+    
   )
 }
 
